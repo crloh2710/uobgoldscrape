a285be4c6c612f42abe86ad983419802c90279fc
crloh2710 <crloh2710@gmail.com>
2026-03-10 20:42:24 +0800

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,2 +1,2 @@
-install.packages(c('netstat', 'tidyverse', 'binman', 'wdman', 'chromote', 'rvest'), 
+install.packages(c('netstat', 'tidyverse', 'wdman', 'chromote', 'rvest'), 
                  repos='https://cloud.r-project.org')
