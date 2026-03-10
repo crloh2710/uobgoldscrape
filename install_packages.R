b40770e2de73828774e922e79932a90a0fd3ee20
crloh2710 <crloh2710@gmail.com>
2026-03-10 21:16:26 +0800

    Update install_packages.R

--- install_packages.R
+++ install_packages.R
@@ -1,2 +1,3 @@
-install.packages(c('netstat', 'tidyverse', 'chromote', 'rvest'), 
+install.packages(c('netstat', 'tidyverse', 'rvest'), 
                  repos='https://cloud.r-project.org')
+remotes::install_github('rstudio/chromote')
